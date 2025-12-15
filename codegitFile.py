e29b0a2aa82890d033aa8838ea2a4df2e8a073ea
SheikhTanzeem <stanzeem172@gmail.com>
2025-12-15 13:49:44 +0530

    Remove merge conflict markers and clean up prints

--- codegitFile.py
+++ codegitFile.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print("hello world , welcome to tanzeem sheikh world ")
-=======
 print("hello world , welcome to tanzeem sheikh world ")
 
 print("my name is tanzeem sheikh ")
@@ -19,9 +16,3 @@ print("my name is tanzeem sheikh ")
 print("hello world , welcome to tanzeem sheikh world ")
 
 print("my name is tanzeem sheikh ")
-
-
-
-
-
->>>>>>> d52385b81b4ac5d3f16f39211bc822e89b667235
